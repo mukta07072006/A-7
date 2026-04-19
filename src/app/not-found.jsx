2531f00ed87b13724170cf6c8b528b2017a425a4
mukta07072006 <worldamity994@gmail.com>
2026-04-19 23:19:29 +0600

    added 404

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+"use client"
+import Link from 'next/link';
+import Lottie from "lottie-react";
+import errorAnimation from "@/assets/error-404.json"; 
 
-const NotFound = () => {
-    return (
-        <div>
-            Not found
-        </div>
-    );
-};
 
-export default NotFound;
+export default function NotFound() {
+  return (
+    <main className="flex min-h-fit flex-col items-center justify-center bg-white p-6 text-center">
+
+      <div className="w-full max-w-sm md:max-w-md lg:max-w-lg">
+        <Lottie 
+          animationData={errorAnimation} 
+          loop={true} 
+          autoplay={true} 
+        />
+      </div>
+    </main>
+  );
+}
